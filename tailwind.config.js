fa04cb8804d79b4aa1f1736b7e1cfff2a8ad6706
khusan2006 <bahodirovkhusan@gmail.com>
2023-10-27 09:28:33 +0500

    changed color of title

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,15 @@ module.exports = {
       mulish: ["Mulish"]
     },
     extend: {
+      colors: {
+        'primary': '#2d2e32'
+      },
       height: {
+        '90': '22rem',
         '100': '26rem'
+      },
+      width: {
+        '90': '22rem',
       },
       boxShadow: {
         'white-md': '0 4px 6px -1px rgba(255, 255, 255, 0.1), 0 2px 4px -1px rgba(255, 255, 255, 0.06)',
