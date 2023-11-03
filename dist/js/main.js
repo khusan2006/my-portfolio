f663217acf79f7e0ff15aab77ff5c6f0a0266e20
khusan2006 <bahodirovkhusan@gmail.com>
2023-11-03 21:15:24 +0500

    added animation to navigation and hero

--- dist/js/main.js
+++ dist/js/main.js
@@ -20,3 +20,47 @@ menuToggler.addEventListener("click", () => {
 })
 
 
+const imgAnimation = anime({
+    targets: '.hero-img',
+    translateY: [50,0],
+    opacity: [0,1],
+    duration: 1000,
+    delay: 1500,
+    easing: 'easeInOutQuad'
+})
+// const navbarAnimation = anime({
+//     targets: '.navbar-item',
+//     translateY: [-100, 0],
+//     opacity: [0,1],
+//     easing: 'linear',
+//     delay: anime.stagger(50, {start: 50})
+// })
+const timeline = anime.timeline({
+    // duration: 1000,
+    easing: 'easeOutExpo',
+  });
+
+  timeline.add({
+    targets: '.navbar-item',
+    translateY: [-100, 0],
+    opacity: [0,1],
+    easing: 'easeOutSine',
+    delay: anime.stagger(50, {start: 50})
+})
+    .add({
+      targets: '#hero-title',
+      translateY: [-100, 0],
+      opacity: [0, 1],
+      duration: 300,
+    }).add({
+        targets: '#hero-subtitle',
+        translateY: [-50, 0],
+        opacity: [0,1],
+        duration: 300,
+
+    }).add({
+        targets: '#icons',
+        translateY: [-30,0],
+        opacity: [0,1],
+        duration: 300,
+    })
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,9 @@ module.exports = {
       width: {
         '90': '22rem',
       },
+      gridTemplateRows: {
+        "25": 'repeat(3,6rem)'
+      },
       boxShadow: {
         'white-md': '0 4px 6px -1px rgba(255, 255, 255, 0.1), 0 2px 4px -1px rgba(255, 255, 255, 0.06)',
         'white-lg': '0 10px 15px -3px rgba(255, 255, 255, 0.1), 0 4px 6px -2px rgba(255, 255, 255, 0.05)',
