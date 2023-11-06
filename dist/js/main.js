a8c05e2ae86df2a2f99e70e15d05886686ba087c
khusan2006 <bahodirovkhusan@gmail.com>
2023-11-06 09:40:02 +0500

    make buttons fuctional

--- dist/js/main.js
+++ dist/js/main.js
@@ -20,20 +20,26 @@ menuToggler.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 })
 
-
-skillsBtn.forEach(btn => {
-    btn.addEventListener('click', function(e) {
-        skillsContainer.forEach((skills, index) => {
-            if(e.target.value !== index) {
-                skills.style.display = 'none'
-            }
-            if(e.target.value == index) {
-                skills.style.display = 'grid'
-            }
+if(document.documentElement.clientWidth < 800) {
+    skillsBtn[0].style.color = 'blue'
+    skillsBtn.forEach(btn => {
+        btn.addEventListener('click', function(e) {
+            skillsContainer.forEach((skills, index) => {
+                if(e.target.value !== index) {
+                    skillsBtn[index].style.color = ''
+                    skills.style.display = 'none'
+                }
+                if(e.target.value == index) {
+                    skillsBtn[index].style.color = 'blue'
+                    skills.style.display = 'grid'
+                }
+            })
+    
         })
-
     })
-})
+}
+
+
 // const imgAnimation = anime({
 //     targets: '.hero-img',
 //     translateY: [-50,0],
